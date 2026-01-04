93739e6c017d8cd4bc1e83eac58d38511d40eeb8
unwavering <morehope586@gmail.com>
2026-01-04 18:21:21 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  website: "https://blog.unwavering.top/", // replace this with your deployed domain
+  author: "Unwavering",
   profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  desc: "自己部署自用Blog",
+  title: "Unwavering-Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
